0720ac8f6a6e3f20e6a45f1d21ae1c632d62cb4e
Marc <marc.spiess@divcom.ch>
2018-07-09 14:59:12 +0200

    Change headers http config

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import router from './router'
 
 Vue.use(VueResource)
 Vue.config.productionTip = false
+Vue.http.headers.common['Content-Type'] = 'application/json'
 Vue.http.headers.common['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9zYW5kYm94LmtvbWFjaGkucG9temVkLmNoXC93cCIsImlhdCI6MTUzMTE0MDEzMSwibmJmIjoxNTMxMTQwMTMxLCJleHAiOjE1MzE3NDQ5MzEsImRhdGEiOnsidXNlciI6eyJpZCI6IjEifX19.15q33tmPivaNRdHI9x3OsEDyiWJJ8un9HkOLvub_ZBo'
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
