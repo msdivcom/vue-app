767bc4b24280bc06ef81a29540aecfcc29c0e589
Marc <marc.spiess@divcom.ch>
2018-07-10 10:15:03 +0200

    Cross origin fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint-disable */
 import Vue from 'vue'
 import App from './App'
 import VueResource from 'vue-resource'
@@ -8,9 +9,9 @@ import router from './router'
 Vue.use(VueResource)
 Vue.config.productionTip = false
 
-Vue.http.headers.common['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9zYW5kYm94LmtvbWFjaGkucG9temVkLmNoXC93cCIsImlhdCI6MTUzMTIwMjk2OCwibmJmIjoxNTMxMjAyOTY4LCJleHAiOjE1MzE4MDc3NjgsImRhdGEiOnsidXNlciI6eyJpZCI6IjEifX19.4yjNvMSq4sqXr8UU285N22XYpTatPdu_kEBeUV6JM80'
-
-Vue.http.headers.common['Access-Control-Expose-Headers'] = 'Authorization'
+Vue.http.headers.common["Content-Type"] = "application/json"
+Vue.http.headers.common["Accept"] = "application/json"
+Vue.http.headers.common["Authorization"] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvc2FuZGJveC5rb21hY2hpLnBvbXplZC5jaFwvd3AiLCJpYXQiOjE1MzEyMTAzNzgsIm5iZiI6MTUzMTIxMDM3OCwiZXhwIjoxNTMxODE1MTc4LCJkYXRhIjp7InVzZXIiOnsiaWQiOiIxIn19fQ.Tu-4Km8AAHB-bYEwYDXWIje0RACeERLLpBan5gjyCxM"
 
 /* eslint-disable no-new */
 new Vue({
